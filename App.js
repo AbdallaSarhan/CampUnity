259d7385a4cd47150dea99f1946f7493f19df187
Abdalla Sarhan <abdalla1@my.yorku.ca>
2023-01-22 16:14:31 -0500

    wrapping children of app in parent hooks for functionality

--- App.js
+++ App.js
@@ -2,11 +2,26 @@ import {NavigationContainer} from '@react-navigation/native';
 import {Text, SafeAreaView} from 'react-native';
 import React from 'react';
 import AppStackNavigator from './app/navigationStacks/AppStackNavigator';
+import {AuthProvider} from './app/hooks/useAuth';
+import {StorageProvider} from './app/hooks/useStorage';
+import {UserProvider} from './app/hooks/useUser';
+import {ServiceProvider} from './app/hooks/useServices';
+import {ProductProvider} from './app/hooks/useProducts';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <AppStackNavigator />
+      <UserProvider>
+        <AuthProvider>
+          <StorageProvider>
+            <ServiceProvider>
+              <ProductProvider>
+                <AppStackNavigator />
+              </ProductProvider>
+            </ServiceProvider>
+          </StorageProvider>
+        </AuthProvider>
+      </UserProvider>
     </NavigationContainer>
   );
 }
